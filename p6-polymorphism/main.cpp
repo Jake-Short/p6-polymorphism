efa3174af90c5c26ecdba421a3110d30a35dc11e
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2022-02-27 13:54:01 -0800

    Initial Commit

---
+++ p6-polymorphism/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  p6-polymorphism
+//
+//  Created by Jake Short on 2/27/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
