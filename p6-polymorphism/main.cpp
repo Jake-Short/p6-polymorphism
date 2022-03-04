304e3b0b87469662dce720a655ef689ccf35ea31
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2022-03-04 14:56:22 -0800

    Added step 6

--- p6-polymorphism/main.cpp
+++ p6-polymorphism/main.cpp
@@ -49,3 +49,21 @@ double Circle::area() const {
     return (_radius * _radius) * M_PI;
 }
 
+/* Reflection
+
+ 1. I think that static polymorphism is easier to implement than dynamic polymorphism. With dynamic polymorphism,
+ we need to have another class in our code (the Shape class), and have all of our classes inherit from that. Then,
+ our Rectangle/Square/Circle classes all need to inherit that class, and override the necessary methods (the area method).
+ However, with static polymorphism, a lot of this code was cut out. We no longer needed the Shape class, so none of our
+ classes needed to inherit it anymore. We could also remove the "override" keyword from the area method. Then, we just made
+ our area method generic (using template/typename). Overall, we could out a lot of code while still achieving the same result.
+ Therefore, I think that static polymorphism is easier and better to implement.
+ 
+ 2. One possible reason for using static polymorphism over dynamic polymorphism is that we are able to cut out a lot of
+ code. As talked about above, static polymorphism was easier to write for this assignment. Furthermore, according to the
+ URL https://blog.feabhas.com/2010/05/polymorphism-in-c/, dynamic polymorphism determines the functions at runtime while
+ static polymorphism determines the functions at compile time. Due to this, static polymorphism may also be faster, since
+ everything is determined at compile time. Therefore, I belive that static polymorphism is encouraged due to the smaller
+ amount of code and determination at compile time.
+ 
+*/
